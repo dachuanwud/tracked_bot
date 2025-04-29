4d49f86ce6820f9f7c04ec9c25906a4dc2700450
lsc <dachuanwudi@gmail.com>
2025-04-29 09:51:11 +0800

    refactor: optimize project structure for flipper model only

--- src/tracked_bot_description/launch/display_flipper.launch.py
+++ src/tracked_bot_description/launch/display_flipper.launch.py
@@ -1,9 +1,7 @@
 from ament_index_python.packages import get_package_share_path
-
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import Command, LaunchConfiguration
-
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
@@ -12,16 +10,19 @@ def generate_launch_description():
     default_model_path = tracked_bot_path / 'urdf/tracked_bot_flipper.urdf'
     default_rviz_config_path = tracked_bot_path / 'rviz/flipper.rviz'
 
-    model_arg = DeclareLaunchArgument(name='model', default_value=str(default_model_path),
-                                      description='Absolute path to robot urdf file')
-    rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=str(default_rviz_config_path),
-                                     description='Absolute path to rviz config file')
+    model_arg = DeclareLaunchArgument(
+        name='model',
+        default_value=str(default_model_path),
+        description='Absolute path to robot urdf file'
+    )
+    
+    rviz_arg = DeclareLaunchArgument(
+        name='rvizconfig',
+        default_value=str(default_rviz_config_path),
+        description='Absolute path to rviz config file'
+    )
 
-    robot_description_content = Command([
-        'xacro ',
-        LaunchConfiguration('model')
-    ])
-
+    robot_description_content = Command(['xacro ', LaunchConfiguration('model')])
     robot_description = {'robot_description': ParameterValue(robot_description_content, value_type=str)}
 
     robot_state_publisher_node = Node(
